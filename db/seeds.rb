852916ba1479e94e0a1b232f85ac55538aeffdfb
irvingvillasenor <irving@pipersolution.com>
2020-05-27 13:29:57 -0400

    Adds products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+# product = Product.new(
+#   name:"Laptop", 
+#   price: 1000, 
+#   image_url:"https://i.pcmag.com/imagery/reviews/038Dr5TVEpwIv8rCljx6UcF-14.fit_scale.size_1028x578.v_1588802180.jpg" , 
+#   description: "Macbook Pro")
+# product.save
+
+# product = Product.new(
+#   name:"Desktop", 
+#   price: 800, 
+#   image_url:"https://www.macworld.co.uk/cmsdata/features/3678783/how-clear-desktop-mac_thumb800.jpg", 
+#   description: "Mac Pro")
+# product.save
+
+product = Product.new(
+  name:"Cell Phone", 
+  price: 1300, 
+  image_url:"https://images.macrumors.com/article-new/2019/09/iphone11prolineup.jpg", 
+  description: "iPhone 11")
+product.save
