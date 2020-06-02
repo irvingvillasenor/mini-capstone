83e650d9e79d7242b56ae1234478c9f0c4e554e4
irvingvillasenor <irving@pipersolution.com>
2020-06-02 14:08:24 -0400

    add rest

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,15 +1,64 @@
 class Api::ProductsController < ApplicationController
 
-def all_products
-  @all_products = Product.all
+  # def all_products
+  #   @all_products = Product.all
 
-  render "products.json.jb"
-  
-end
+  #   render "products.json.jb"
+    
+  # end
 
-def laptop_action
-  @laptop = Product.find_by(name: "Laptop")
-  render "laptop.json.jb"
-end
+  # def laptop_action
+  #   @laptop = Product.find_by(name: "Laptop")
+  #   render "laptop.json.jb"
+  # end
+
+  # def single_product
+  #   @single_product = Product.sample
+  #   render "single_product.json.jb"
+
+  # end
+
+  def index
+    @all_products = Product.all
+    render "index.json.jb"
+    
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    render "show.json.jb"
+    
+  end
+
+  def create
+    @product = Product.new(
+      name: params[:name], 
+      price: params[:price], 
+      image_url: params[:image_url], 
+      description: params[:description] 
+    )
+    @product.save
+    render "show.json.jb"
+    
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id] )
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
+    @product.save
+    render "show.json.jb"
+    
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    render "destroy.json.jb"
+    
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-    get "/products" => "products#all_products"
-    get "/laptop" => "products#laptop_action"
+    # get "/products" => "products#all_products"
+    # get "/laptop" => "products#laptop_action"
+    get "/products" => "products#index"
+    get "/products/:id" => "products#show"
+    post "/products" => "products#create"
+    patch "products/:id" => "products#update" 
+    delete "/products/:id" => "products#destroy"
   end
 end
